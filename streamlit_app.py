0a56c3ba764a4d526005cb6a38496947a0fc2bc7
eapo <e@eaposztrof.com>
2024-12-18 06:18:04 +0000

    init

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("📄 Document question answering")
+st.title("📄 DoQ&A")
 st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+    "Upload a document and ask about it – GPT will answer!"
+    "Provide an [OpenAI API key](https://platform.openai.com/account/api-keys)."
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
@@ -26,8 +26,8 @@ else:
 
     # Ask the user for a question via `st.text_area`.
     question = st.text_area(
-        "Now ask a question about the document!",
-        placeholder="Can you give me a short summary?",
+        "Questions about the document:",
+        value="Give me a short summary with ordered lists.",
         disabled=not uploaded_file,
     )
 
